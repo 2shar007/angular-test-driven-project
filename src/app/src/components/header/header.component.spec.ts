c04b5debd1359a34b9e549ec52280e71416a212f
2shar007 <2shar00ghodke@gmail.com>
2022-03-31 12:29:31 +0530

    changes made for Navigation Bar and added data-test attributes

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -19,7 +19,26 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display logo', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
   });
+  
+  it('should display search', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="search"]')).toBeTruthy();
+  });
+
+  it('should display menu', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="menu"]')).toBeTruthy();
+  });
+  it('should display filters', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="home-type"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="dates"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="guests"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="price"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="rooms"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="amenities"]')).toBeTruthy();
+
+  });
+
 });
