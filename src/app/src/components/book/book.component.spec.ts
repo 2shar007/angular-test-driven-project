0af0a86480819b2b587466d535a718a16f3a8aa8
2shar007 <2shar00ghodke@gmail.com>
2022-04-05 11:11:49 +0530

    changes added for Book Components and adding unit test

--- src/app/src/components/book/book.component.spec.ts
+++ src/app/src/components/book/book.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { BookComponent } from './book.component';
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
+  let dialogData;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
+    dialogData = TestBed.get(MAT_DIALOG_DATA);
     component = fixture.componentInstance;
+
+    const homes = require('../../../../assets/homes.json');
+    dialogData.home = homes[0];
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show title', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="title"]').textContent).toContain('Home 1');
   });
 });
