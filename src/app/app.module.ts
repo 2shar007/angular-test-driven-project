cc72040a0e94f97c8241b791e81a4498bc4812b1
2shar007 <2shar00ghodke@gmail.com>
2022-04-05 10:49:23 +0530

    changes added for Book Componenets and added Mat Dialog changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './src/components/header/header.component';
 import { HomesComponent } from './src/components/homes/homes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BookComponent } from './src/components/book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomesComponent
+    HomesComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    BookComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
